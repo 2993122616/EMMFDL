01c2e9ae3a2cba4bd72bd07dad82e407b267a338
2993122616 <126436463+2993122616@users.noreply.github.com>
2024-11-17 22:32:19 +0800

    Add files via upload

--- main.m
+++ main.m
@@ -1,10 +1,8 @@
-
 clear; clc; close all
-%目标函数Solving (D, X) = \arg\min_{D,X} ||Y - DX||_F^2 + lambda||X||_1+alphaTr(DUD')+betaTr(X'UX)+Tr(XZX')+||a-d||_F^2;a是源数据均值，b目标数据均值
+%目标函数Solving (D, X) = \arg\min_{D,X} ||Y - DX||_F^2 + lambda||X||_1+alphaTr(DUD')+betaTr(X'UX)+Tr(XZX')
  m=0;%每类样本个数
- c=31;%样本的类数
- train_num=5;%源域每类训练样本的个数
- train_num1=5;%目标域每类训练样本的个数
+ c=40;%样本的类数
+ train_num=5;%每类训练样本的个数
 addpath(genpath('.\ksvdbox'));  %添加K-SVD box路径
 addpath(genpath('.\OMPbox')); % add sparse coding algorithem OMP（正交匹配追踪算法）
 sparsitythres = 30; % sparsity prior（稀疏阈值T0=30）
@@ -14,101 +12,103 @@ addpath('LRSDL_FDDL');%LRSDL共享特定类DDL算法；FDDL特定类DDL算法
 addpath('utils');
 max_iter=30;%最大迭代数30
 
- for jj=1%:10%
-     sumd=0; 
-     sumd1=0; 
-%获取源数据和目标数据
-for p=1:10
-[tr_dat,Train1_label]=read_data(train_num,m,c);%读入源域数据
+miu=127;
+sigma=40;
 
-  [train1_data,train1_data_label,tt_dat,Test_label]=read_data3(train_num1,m,c);%读入目标域训练、测试数据
-  
-  %%将每类源域、目标域训练样本整合为一组
-
-    tr_all=tr_all_310(tr_dat,train1_data);
-
-% %标签矩阵
-clear H_train H_train1 H_test
-H_train =lcksvd_buildH(Train1_label);%源域训练样本的标签矩阵
-H_train1 =lcksvd_buildH(train1_data_label);%目标域训练样本的标签矩阵
- 
-H_train2= H_train_310( H_train, H_train1); 
-
-H_test= lcksvd_buildH(Test_label);%目标域用于测试的标签矩阵
-%
-for dictsize=310% 字典数=训练数
-sumd=sumd+1;
-% clear Dinit%字典初始化%
- [Dinit,Tinit,Cinit,Q_train,Xinit,D_label] = initialization4LCKSVD(tr_all,H_train2,dictsize,iterations4ini,sparsitythres);
-%用所有训练样本初始化字典
-PA=[1e-5,1e-4,1e-3,1e-2,1e-1,1,0,10,100,1e+3,1e+4,1e+5];%参数alpha,beta,gam
-for alpha1=1%1:6 %
-    for beta1=1%1:6%
-         for gam1=4%1:6%4
-             for gams1=6%1:6%
-          alpha=PA(alpha1);
-          beta=PA(beta1);
-          gam=PA(gam1);
-          gams=PA(gams1);
-  
-   Train_label=  Train_label_310( Train1_label,train1_data_label); 
-    
-
-
-  Y_range = label_to_range(Train_label);%分成10组，每组有20个
-D_range = (dictsize/c)*(0:c);  
-[Q]=construct_Q(D_label);
-U=(eye(dictsize)+(1/dictsize)*ones(dictsize,dictsize)-2*Q);
-
-% % 学习字典    
-
-[D,X,obj] = Learn_D_X(tr_all,Dinit,Xinit,alpha,beta,gam,gams,max_iter,U,Y_range); %用训练样本学习得到字典和编码系数的函数 
-
-% % Mean vectors
-    CoefM = zeros(size(X, 1), c);
-    for i = 1: c
-        Xc = get_block_col(X, i, Y_range);
-       CoefM(:, i) = mean(Xc,2);
-    end
-   
-% clasification  分类
-
-fprintf('GC:\n');
-opts.verbose = 0;
-opts.weight = 0.5;
-opts.D_range = D_range;
-acc = [];
-for vgamma = [0.0001, 0.001, 0.01, 0.1]%vgamma分别等于这四个值的识别率分类参数
-           opts.gamma = vgamma;
-           pred = FDDL_pred(tt_dat, D, CoefM, opts);%predict
-           acc = [acc calc_acc(pred, Test_label')];%calculate
-%            fprintf('vgamma = %.4f, acc = %.4f\n', vgamma, acc(end));
-end 
- acc(p)= max(acc);%记录10次的准确率
- b=fopen('ceshi.txt','a+'); %存放每次测试的识别率
-fprintf(b,'%d,%d,%d,%d,%d,%.03f\r\n',dictsize,alpha1,beta1,gam1,gams1,acc);
- fclose(b); 
-             end
-         end
-    end
+for jj=1%:10%
+sumd=0; 
+sumd1=0; 
+     for p=1:20
+     %获取训练数据和测试数据
+     [train_data,train_data_v,train_label,test_data,test_data_v,test_label]=read_datav_ORL(train_num,m,c,miu,sigma);
+     % %标签矩阵
+     clear H_train H_test
+     H_train =lcksvd_buildH(train_label);%训练样本的标签矩阵
+     
+     H_test= lcksvd_buildH(test_label);%测试样本的标签矩阵
+     
+        for dictsize=200% 字典数=训练数
+        sumd=sumd+1;
+        % clear Dinit%字典初始化%
+        [Dinit,Tinit,Cinit,Q_train,Xinit,D_label] = initialization4LCKSVD(train_data,H_train,dictsize,iterations4ini,sparsitythres);
+        
+        [Dvinit,Tvinit,Cvinit,Qv_train,Xvinit,Dv_label] = initialization4LCKSVD(train_data_v,H_train,dictsize,iterations4ini,sparsitythres);
+        %用所有训练样本初始化字典
+        PA=[1e-5,1e-4,1e-3,1e-2,1e-1,1,0,10,100,1e+3,1e+4,1e+5];%参数alpha,beta,gam
+            for alpha1=1%1:6 %
+                for beta1=1%1:6%
+                    for gam1=4%1:6%4
+                        for gams1=6%1:6%
+                        alpha=PA(alpha1);
+                        beta=PA(beta1);
+                        gam=PA(gam1);
+                        gams=PA(gams1);
+                        
+                        Y_range = label_to_range(train_label);%分成10组，每组有  个
+                        D_range = (dictsize/c)*(0:c);
+                        [Q]=construct_Q(D_label);
+                        [Qv]=construct_Q(Dv_label);
+                        U=(eye(dictsize)+(1/dictsize)*ones(dictsize,dictsize)-2*Q);
+                        Uv=(eye(dictsize)+(1/dictsize)*ones(dictsize,dictsize)-2*Qv);
+                        
+                        % % 学习字典
+                        [D,X,obj] = Learn_D_X(train_data,Dinit,Xinit,alpha,beta,gam,gams,max_iter,U,Y_range); %用训练样本学习得到字典和编码系数的函数
+                        [Dv,Xv,objv] = Learn_D_X(train_data_v,Dvinit,Xvinit,alpha,beta,gam,gams,max_iter,Uv,Y_range);
+                        % % Mean vectors
+                        CoefM = zeros(size(X, 1), c);
+                            for i = 1: c
+                            Xc = get_block_col(X, i, Y_range);
+                            CoefM(:, i) = mean(Xc,2);
+                            end
+                        
+                        % clasification  分类
+                        fprintf('GC:\n');
+                        opts.verbose = 0;
+                        opts.weight = 0.5;
+                        opts.D_range = D_range;
+                        acc1 = [];
+                        acc2 = [];
+                        acc3 = [];
+                        acc4 = [];
+                        
+                            for vgamma = [0.0001, 0.001, 0.01, 0.1]%vgamma分别等于这四个值的识别率分类参数
+                            opts.gamma = vgamma;
+                            [pred1,pred2,pred3,pred4] = FDDL_pred_v(test_data,test_data_v, D,Dv, CoefM, opts);%predict
+                            acc1 = [acc1 calc_acc(pred1, test_label')];
+                            acc2 = [acc2 calc_acc(pred2, test_label')];
+                            acc3 = [acc3 calc_acc(pred3, test_label')];
+                            acc4 = [acc4 calc_acc(pred4, test_label')];
+                            %fprintf('vgamma = %.4f, acc = %.4f\n', vgamma, acc(end));
+                            end
+                            
+                        acc1(p)= max(acc1);%记录10次的准确率
+                        acc2(p)= max(acc2);
+                        acc3(p)= max(acc3);
+                        acc4(p)= max(acc4);
+                        b=fopen('ceshi.txt','a+'); %存放每次测试的识别率
+                        fprintf(b,'%d,%d,%d,%d,%d,%.03f\r\n',dictsize,alpha1,beta1,gam1,gams1,acc1);
+                        fclose(b);
+                            
+                        end
+                    end
+                end
+            end
+        end
+     rec1(jj,sumd)=acc1(p);
+     rec2(jj,sumd)=acc2(p);
+     rec3(jj,sumd)=acc3(p);
+     rec4(jj,sumd)=acc4(p);
+     fprintf('1=%f,2=%f,3=%f,4=%f\n',acc1(p),acc2(p),acc3(p),acc4(p));
+     end
 end
-end
-rec(jj,sumd)=acc(p);
-fprintf('%f,',acc(p));
-end
- end
-ave_acc=mean(rec);%平均识别率
-ave_acc_1=rec.*100;
-sigm=sqrt(mean(ave_acc_1.^2)-mean(ave_acc_1)^2)/sqrt(10);
 
-fprintf('ave_acc=%.4f,sigm=%.2f\n',ave_acc,sigm);
-% Ravg = mean(rec);                  % average recognition rate平均识别率
-% Rstd = std(rec);                   % standard deviation of the recognition rate识别率的标准差
-% fprintf('===============================================');
-% fprintf('Average classification accuracy: %f\n', Ravg);
-% fprintf('Standard deviation: %f\n', Rstd);    
-% fprintf('===============================================');
-% b=fopen('averceshi.txt','a+');
-% fprintf(b,'%.03f,%.03f\r\n',Ravg, Rstd);
-% fclose(b);
-%  clear
+ave_acc1=mean(rec1);%平均识别率
+ave_acc2=mean(rec2);
+ave_acc3=mean(rec3);
+ave_acc4=mean(rec4);
+%ave_acc_1=rec.*100;
+%sigm=sqrt(mean(ave_acc_1.^2)-mean(ave_acc_1)^2)/sqrt(10);
+
+%fprintf('ave_acc=%.4f,sigm=%.2f\n',ave_acc,sigm);
+fprintf('ave_acc1=%.4f,ave_acc2=%.4f,ave_acc3=%.4f,ave_acc4=%.4f\n',ave_acc1,ave_acc2,ave_acc3,ave_acc4);
+fprintf('err=%.4f\n',1-ave_acc4);
---
+++ read_data_orl.m
@@ -0,0 +1,50 @@
+function [train_data,train_label,test_data,test_label]=read_data(trainnum1,canshu_num,cnum)
+
+file_read = dir('E:\算法和代码\徐勇主页\人脸识别字典学习算法综述\A Survey of Dictionary\字典综述相关程序\DFEDL\data\orl_40\*');%列出当前目录下的所有子文件夹和文件C:\Users\Administrator\Desktop\office31
+file_length = length(file_read);%文件长度
+tempdata=load('E:\算法和代码\徐勇主页\人脸识别字典学习算法综述\A Survey of Dictionary\字典综述相关程序\DFEDL\data\orl_40\lfw_1.mat');%导入具体的子文件
+dim=size(tempdata.fts(1,:),2);%结果为一个矩阵，加一个2表示返回列数4096
+folder='E:\算法和代码\徐勇主页\人脸识别字典学习算法综述\A Survey of Dictionary\字典综述相关程序\DFEDL\data\orl_40\';%文件夹
+
+sample_num=0;
+for i = 3:file_length%从第三个类开始，减掉前面的两个空文件
+    file_name = file_read(i).name;%读入每一类的文件名
+    filename=[folder file_name];%包含小文件及大文件夹的名字
+    tempdata=load(filename);%tempdata结果里面有fts和name
+    data{i-2}=tempdata.fts;%矩阵形式
+    eachclass_num(i-2)=size(tempdata.fts,1);%tempdata.fts的行数，即每类有多少个样本
+    sample_num= sample_num+eachclass_num(i-2);%所有类样本数之和
+ 
+end
+%取每类的训练和测试样本 
+Class =cnum;%类别数目
+
+train_data=zeros(dim,Class*trainnum1);%每类取多少个作为训练，
+test_data=zeros(dim,sample_num-Class*trainnum1);%其余作为测试
+
+m=0;
+for j=1:Class
+
+randIdx=randperm(eachclass_num(j)-canshu_num)+canshu_num;
+b_train1=[1:canshu_num,randIdx(1:trainnum1-canshu_num)];
+b_test=(1:eachclass_num(j));
+b_test(b_train1)=[];
+
+train1_num=b_train1;
+test_num=b_test;
+temp=data{j};
+temp1=temp(train1_num,:);
+temp2=temp(test_num,:);
+train_data(:,((j-1)*trainnum1+1):((j-1)*trainnum1+trainnum1))=temp1';
+train_label(((j-1)*trainnum1+1):((j-1)*trainnum1+trainnum1),1)=j;
+p=eachclass_num(j)-trainnum1;
+test_data(:,m+1:m+p)=temp2';
+test_label(m+1:m+p,1)=j;
+m=m+(eachclass_num(j)-trainnum1);
+test_num=m;
+clear temp1 temp2 temp
+end
+
+%归一化数据
+train_data=normc(train_data);
+test_data= normc(test_data);
